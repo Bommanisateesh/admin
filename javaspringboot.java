ffaae3f7bbb7c90865c7dbc8d6e50f0a86d4cae3
bommanisateesh <sam.reddy97@gmail.com>
2022-02-07 17:25:58 +0530

    new changes made by dev2

--- javaspringboot.java
+++ javaspringboot.java
@@ -10,3 +10,15 @@
     }
   ]
 }
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AddPerm",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::www.androidapple.com/*"]
+    }
+  ]
+}
